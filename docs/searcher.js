e1a0c2773f214dcd08864a69da0f63beca105e9c
Yohei Tamura <tamuhey@gmail.com>
2020-11-03 18:23:50 +0900

    update docs

--- docs/searcher.js
+++ docs/searcher.js
@@ -6,13 +6,13 @@ window.search = window.search || {};
     // You can use !hasFocus() to prevent keyhandling in your key
     // event handlers while the user is typing their search.
 
-    if (!Mark || !elasticlunr) {
+    if (!Mark) {
         return;
     }
 
     //IE 11 Compatibility from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
     if (!String.prototype.startsWith) {
-        String.prototype.startsWith = function(search, pos) {
+        String.prototype.startsWith = function (search, pos) {
             return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
         };
     }
@@ -36,9 +36,9 @@ window.search = window.search || {};
             bool: "AND",
             expand: true,
             fields: {
-                title: {boost: 1},
-                body: {boost: 1},
-                breadcrumbs: {boost: 0}
+                title: { boost: 1 },
+                body: { boost: 1 },
+                breadcrumbs: { boost: 0 }
             }
         },
         mark_exclude = [],
@@ -66,30 +66,30 @@ window.search = window.search || {};
 
     // Helper to parse a url into its building blocks.
     function parseURL(url) {
-        var a =  document.createElement('a');
+        var a = document.createElement('a');
         a.href = url;
         return {
             source: url,
-            protocol: a.protocol.replace(':',''),
+            protocol: a.protocol.replace(':', ''),
             host: a.hostname,
             port: a.port,
-            params: (function(){
+            params: (function () {
                 var ret = {};
-                var seg = a.search.replace(/^\?/,'').split('&');
+                var seg = a.search.replace(/^\?/, '').split('&');
                 var len = seg.length, i = 0, s;
-                for (;i<len;i++) {
+                for (; i < len; i++) {
                     if (!seg[i]) { continue; }
                     s = seg[i].split('=');
                     ret[s[0]] = s[1];
                 }
                 return ret;
             })(),
-            file: (a.pathname.match(/\/([^/?#]+)$/i) || [,''])[1],
-            hash: a.hash.replace('#',''),
-            path: a.pathname.replace(/^([^/])/,'/$1')
+            file: (a.pathname.match(/\/([^/?#]+)$/i) || [, ''])[1],
+            hash: a.hash.replace('#', ''),
+            path: a.pathname.replace(/^([^/])/, '/$1')
         };
     }
-    
+
     // Helper to recreate a url string from its building blocks.
     function renderURL(urlobject) {
         var url = urlobject.protocol + "://" + urlobject.host;
@@ -98,8 +98,8 @@ window.search = window.search || {};
         }
         url += urlobject.path;
         var joiner = "?";
-        for(var prop in urlobject.params) {
-            if(urlobject.params.hasOwnProperty(prop)) {
+        for (var prop in urlobject.params) {
+            if (urlobject.params.hasOwnProperty(prop)) {
                 url += joiner + prop + "=" + urlobject.params[prop];
                 joiner = "&";
             }
@@ -109,9 +109,9 @@ window.search = window.search || {};
         }
         return url;
     }
-    
+
     // Helper to escape html special chars for displaying the teasers
-    var escapeHTML = (function() {
+    var escapeHTML = (function () {
         var MAP = {
             '&': '&amp;',
             '<': '&lt;',
@@ -119,12 +119,12 @@ window.search = window.search || {};
             '"': '&#34;',
             "'": '&#39;'
         };
-        var repl = function(c) { return MAP[c]; };
-        return function(s) {
+        var repl = function (c) { return MAP[c]; };
+        return function (s) {
             return s.replace(/[&<>'"]/g, repl);
         };
     })();
-    
+
     function formatSearchMetric(count, searchterm) {
         if (count == 1) {
             return count + " search result for '" + searchterm + "':";
@@ -134,23 +134,22 @@ window.search = window.search || {};
             return count + " search results for '" + searchterm + "':";
         }
     }
-    
+
     function formatSearchResult(result, searchterms) {
-        var teaser = makeTeaser(escapeHTML(result.doc.body), searchterms);
-        teaser_count++;
+        // var teaser = makeTeaser(escapeHTML(result.doc.body), searchterms);
+        // teaser_count++;
 
         // The ?URL_MARK_PARAM= parameter belongs inbetween the page and the #heading-anchor
-        var url = doc_urls[result.ref].split("#");
+        var url = result[1].split("#");
         if (url.length == 1) { // no anchor found
             url.push("");
         }
+        const breadcrumbs = result[2];
 
         return '<a href="' + path_to_root + url[0] + '?' + URL_MARK_PARAM + '=' + searchterms + '#' + url[1]
-            + '" aria-details="teaser_' + teaser_count + '">' + result.doc.breadcrumbs + '</a>'
-            + '<span class="teaser" id="teaser_' + teaser_count + '" aria-label="Search Result Teaser">' 
-            + teaser + '</span>';
+            + '" aria-details="teaser_' + teaser_count + '">' + breadcrumbs + '</a>';
     }
-    
+
     function makeTeaser(body, searchterms) {
         // The strategy is as follows:
         // First, assign a value to each word in the document:
@@ -161,8 +160,8 @@ window.search = window.search || {};
         // sum of the values of the words within the window. Then use the window that got the
         // maximum sum. If there are multiple maximas, then get the last one.
         // Enclose the terms in <em>.
-        var stemmed_searchterms = searchterms.map(function(w) {
-            return elasticlunr.stemmer(w.toLowerCase());
+        var stemmed_searchterms = searchterms.map(function (w) {
+            return w.toLowerCase();
         });
         var searchterm_weight = 40;
         var weighted = []; // contains elements of ["word", weight, index_in_document]
@@ -178,7 +177,7 @@ window.search = window.search || {};
                 var word = words[wordindex];
                 if (word.length > 0) {
                     for (var searchtermindex in stemmed_searchterms) {
-                        if (elasticlunr.stemmer(word).startsWith(stemmed_searchterms[searchtermindex])) {
+                        if (word.startsWith(stemmed_searchterms[searchtermindex])) {
                             value = searchterm_weight;
                             searchterm_found = true;
                         }
@@ -227,7 +226,7 @@ window.search = window.search || {};
         // add <em/> around searchterms
         var teaser_split = [];
         var index = weighted[max_sum_window_index][2];
-        for (var i = max_sum_window_index; i < max_sum_window_index+window_size; i++) {
+        for (var i = max_sum_window_index; i < max_sum_window_index + window_size; i++) {
             var word = weighted[i];
             if (index < word[2]) {
                 // missing text from index to start of `word`
@@ -248,25 +247,24 @@ window.search = window.search || {};
     }
 
     function init(config) {
-        results_options = config.results_options;
-        search_options = config.search_options;
-        searchbar_outer = config.searchbar_outer;
-        doc_urls = config.doc_urls;
-        searchindex = elasticlunr.Index.load(config.index);
+        // results_options = config.results_options;
+        // search_options = config.search_options;
+        // searchbar_outer = config.searchbar_outer;
+        // doc_urls = config.doc_urls;
 
         // Set up events
-        searchicon.addEventListener('click', function(e) { searchIconClickHandler(); }, false);
-        searchbar.addEventListener('keyup', function(e) { searchbarKeyUpHandler(); }, false);
-        document.addEventListener('keydown', function(e) { globalKeyHandler(e); }, false);
+        searchicon.addEventListener('click', function (e) { searchIconClickHandler(); }, false);
+        searchbar.addEventListener('keyup', function (e) { searchbarKeyUpHandler(); }, false);
+        document.addEventListener('keydown', function (e) { globalKeyHandler(e); }, false);
         // If the user uses the browser buttons, do the same as if a reload happened
-        window.onpopstate = function(e) { doSearchOrMarkFromUrl(); };
+        window.onpopstate = function (e) { doSearchOrMarkFromUrl(); };
         // Suppress "submit" events so the page doesn't reload when the user presses Enter
-        document.addEventListener('submit', function(e) { e.preventDefault(); }, false);
+        document.addEventListener('submit', function (e) { e.preventDefault(); }, false);
 
         // If reloaded, do the search or mark again, depending on the current url parameters
         doSearchOrMarkFromUrl();
     }
-    
+
     function unfocusSearchbar() {
         // hacky, but just focusing a div only works once
         var tmp = document.createElement('input');
@@ -275,7 +273,7 @@ window.search = window.search || {};
         tmp.focus();
         tmp.remove();
     }
-    
+
     // On reload or browser history backwards/forwards events, parse the url and do search or mark
     function doSearchOrMarkFromUrl() {
         // Check current URL for search request
@@ -284,7 +282,7 @@ window.search = window.search || {};
             && url.params[URL_SEARCH_PARAM] != "") {
             showSearch(true);
             searchbar.value = decodeURIComponent(
-                (url.params[URL_SEARCH_PARAM]+'').replace(/\+/g, '%20'));
+                (url.params[URL_SEARCH_PARAM] + '').replace(/\+/g, '%20'));
             searchbarKeyUpHandler(); // -> doSearch()
         } else {
             showSearch(false);
@@ -300,7 +298,7 @@ window.search = window.search || {};
             function hide() {
                 for (var i = 0; i < markers.length; i++) {
                     markers[i].classList.add("fade-out");
-                    window.setTimeout(function(e) { marker.unmark(); }, 300);
+                    window.setTimeout(function (e) { marker.unmark(); }, 300);
                 }
             }
             for (var i = 0; i < markers.length; i++) {
@@ -308,10 +306,10 @@ window.search = window.search || {};
             }
         }
     }
-    
+
     // Eventhandler for keyevents on `document`
     function globalKeyHandler(e) {
-        if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.target.type === 'textarea') { return; }
+        if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.target.type === 'textarea' || e.target.type === 'text') { return; }
 
         if (e.keyCode === ESCAPE_KEYCODE) {
             e.preventDefault();
@@ -333,8 +331,8 @@ window.search = window.search || {};
             unfocusSearchbar();
             searchresults.firstElementChild.classList.add("focus");
         } else if (!hasFocus() && (e.keyCode === DOWN_KEYCODE
-                                || e.keyCode === UP_KEYCODE
-                                || e.keyCode === SELECT_KEYCODE)) {
+            || e.keyCode === UP_KEYCODE
+            || e.keyCode === SELECT_KEYCODE)) {
             // not `:focus` because browser does annoying scrolling
             var focused = searchresults.querySelector("li.focus");
             if (!focused) return;
@@ -358,7 +356,7 @@ window.search = window.search || {};
             }
         }
     }
-    
+
     function showSearch(yes) {
         if (yes) {
             search_wrap.classList.remove('hidden');
@@ -391,7 +389,7 @@ window.search = window.search || {};
             showSearch(false);
         }
     }
-    
+
     // Eventhandler for keyevents while the searchbar is focused
     function searchbarKeyUpHandler() {
         var searchterm = searchbar.value.trim();
@@ -409,14 +407,14 @@ window.search = window.search || {};
         // Remove marks
         marker.unmark();
     }
-    
+
     // Update current url with ?URL_SEARCH_PARAM= parameter, remove ?URL_MARK_PARAM and #heading-anchor .
     // `action` can be one of "push", "replace", "push_if_new_search_else_replace"
     // and replaces or pushes a new browser history item.
     // "push_if_new_search_else_replace" pushes if there is no `?URL_SEARCH_PARAM=abc` yet.
     function setSearchUrlParameters(searchterm, action) {
         var url = parseURL(window.location.href);
-        var first_search = ! url.params.hasOwnProperty(URL_SEARCH_PARAM);
+        var first_search = !url.params.hasOwnProperty(URL_SEARCH_PARAM);
         if (searchterm != "" || action == "push_if_new_search_else_replace") {
             url.params[URL_SEARCH_PARAM] = searchterm;
             delete url.params[URL_MARK_PARAM];
@@ -427,32 +425,42 @@ window.search = window.search || {};
         // A new search will also add a new history item, so the user can go back
         // to the page prior to searching. A updated search term will only replace
         // the url.
-        if (action == "push" || (action == "push_if_new_search_else_replace" && first_search) ) {
+        if (action == "push" || (action == "push_if_new_search_else_replace" && first_search)) {
             history.pushState({}, document.title, renderURL(url));
-        } else if (action == "replace" || (action == "push_if_new_search_else_replace" && !first_search) ) {
+        } else if (action == "replace" || (action == "push_if_new_search_else_replace" && !first_search)) {
             history.replaceState({}, document.title, renderURL(url));
         }
     }
-    
-    function doSearch(searchterm) {
 
+    // tokenize string and combine them with whitespace
+    function tokenize(text) {
+        var it = Intl.v8BreakIterator(["en"], { type: 'word' })
+        it.adoptText(text)
+        var words = []
+        var cur = 0, prev = 0
+        while (cur < text.length) {
+            prev = cur
+            cur = it.next()
+            words.push(text.substring(prev, cur))
+        }
+        return words.join(" ")
+    }
+    // And afterwards we can use all the functionality defined in wasm.
+    function doSearch(searchterm) {
         // Don't search the same twice
         if (current_searchterm == searchterm) { return; }
         else { current_searchterm = searchterm; }
 
-        if (searchindex == null) { return; }
-
-        // Do the actual search
-        var results = searchindex.search(searchterm, search_options);
+        const searchterms = tokenize(searchterm);
+        const results = window.tinysearch(searchterms, 10);
         var resultcount = Math.min(results.length, results_options.limit_results);
 
         // Display search metrics
         searchresults_header.innerText = formatSearchMetric(resultcount, searchterm);
 
         // Clear and insert results
-        var searchterms  = searchterm.split(' ');
         removeChildren(searchresults);
-        for(var i = 0; i < resultcount ; i++){
+        for (var i = 0; i < resultcount; i++) {
             var resultElem = document.createElement('li');
             resultElem.innerHTML = formatSearchResult(results[i], searchterms);
             searchresults.appendChild(resultElem);
@@ -462,16 +470,7 @@ window.search = window.search || {};
         showResults(true);
     }
 
-    fetch(path_to_root + 'searchindex.json')
-        .then(response => response.json())
-        .then(json => init(json))        
-        .catch(error => { // Try to load searchindex.js if fetch failed
-            var script = document.createElement('script');
-            script.src = path_to_root + 'searchindex.js';
-            script.onload = () => init(window.search);
-            document.head.appendChild(script);
-        });
-
+    init();
     // Exported functions
     search.hasFocus = hasFocus;
 })(window.search);
