4caa371d8ab342af7a616284c9684748ee624db5
Yohei Tamura <tamuhey@gmail.com>
2020-11-03 19:17:04 +0900

    fix for github pages

--- docs/init.js
+++ docs/init.js
@@ -1,8 +1,8 @@
-import { search as tinysearch, default as init } from "/tinysearch_engine.js";
+import { search as tinysearch, default as init } from "/book-ja/tinysearch_engine.js";
 window.tinysearch = tinysearch;
 
 async function run() {
-    await init('/tinysearch_engine_bg.wasm');
+    await init('/book-ja/tinysearch_engine_bg.wasm');
 }
 
 run();
